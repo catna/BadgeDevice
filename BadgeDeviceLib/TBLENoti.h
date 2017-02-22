b1ebbee6d5d00c83dc545f53e44d871b48e2e1fc
catna <catnaer@163.com>
2017-02-22 13:43:21 +0800

    UPDATE:add noti file and update setting

---
+++ BadgeDeviceLib/TBLENoti.h
@@ -0,0 +1,13 @@
+//
+//  TBLENoti.h
+//  BadgeDevice
+//
+//  Created by MX on 2017/2/22.
+//  Copyright © 2017年 mx. All rights reserved.
+//
+
+#ifndef TBLENoti_h
+#define TBLENoti_h
+
+
+#endif /* TBLENoti_h */
---
+++ BadgeDeviceLib/TBLENoti.m
@@ -0,0 +1,9 @@
+//
+//  TBLENoti.m
+//  BadgeDevice
+//
+//  Created by MX on 2017/2/22.
+//  Copyright © 2017年 mx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
